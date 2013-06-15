2f1382741064f7644a129125ce67293603698ed9
Brian C Hamrick <bhamrick@mit.edu>
2013-06-15 04:14:30 -0400

    Testing out config setting

--- leaderboard/__init__.py
+++ leaderboard/__init__.py
@@ -1,11 +1,14 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+import os
 
 from .config import routes
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    if settings['sqlalchemy.url'][0] == '%':
+        settings['sqlalchemy.url'] = os.environ[settings['sqlalchemy.url'][1:]]
     engine = engine_from_config(settings, 'sqlalchemy.')
     globals()['DBEngine'] = engine
     config = Configurator(settings=settings)
